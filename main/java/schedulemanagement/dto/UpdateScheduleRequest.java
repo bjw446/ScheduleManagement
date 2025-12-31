40b7b49fe7e298566281e7a3ef95834b1ccd3a2f
bjw446 <bjw446@naver.com>
2025-12-31 20:49:02 +0900

    일정 수정, 일정 삭제 및 비밀번호 등 조건 추가

---
+++ main/java/schedulemanagement/dto/UpdateScheduleRequest.java
@@ -0,0 +1,13 @@
+package schedulemanagement.dto;
+
+
+import lombok.Getter;
+
+@Getter
+public class UpdateScheduleRequest {
+
+    private String title;
+    private String name;
+    private String password;
+
+}
